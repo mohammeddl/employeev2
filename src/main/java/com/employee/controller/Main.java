27d58be166dc2e3a6ad298a2fc2e8499ed4beb29
mohammeddl <mohammed_daali2@um5.ac.ma>
2024-10-12 23:52:56 +0100

    fix inputs style

--- src/main/java/com/employee/controller/Main.java
+++ src/main/java/com/employee/controller/Main.java
@@ -1,4 +1,5 @@
 package com.employee.controller;
+import javax.mail.MessagingException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -6,6 +7,7 @@ import javax.persistence.Persistence;
 import com.employee.model.Admin;
 import com.employee.model.Employee;
 import com.employee.model.Recruiter;
+import com.employee.util.EmailService;
 
 import java.util.Date;
 public class Main {
@@ -13,18 +15,33 @@ public class Main {
 
     public static void main(String[] args) {
         
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("MyPU");
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
+        // EntityManagerFactory emf = Persistence.createEntityManagerFactory("MyPU");
+        // EntityManager em = emf.createEntityManager();
+        // em.getTransaction().begin();
 
         // Employee employee = new Employee("John Doe", "tes@gmail.com", "password" , "EMPLOYEE", new Date(), "2023-10-01", 50000.0, "Developer", "IT", "123456789", "555-1234", "123 Street");
 
         // Admin admin = new Admin("daali", "daali@admin.com", "password", "ADMIN", new Date(), "2023-10-01", "hay mghib arabi ");
-        Recruiter recruiter = new Recruiter("med", "dfdf@test.com", "password", "RECRUITER", new Date(), "2023-10-01", "hay mghib arabi");
-        em.persist(recruiter);
+        // Recruiter recruiter = new Recruiter("med", "dfdf@test.com", "password", "RECRUITER", new Date(), "2023-10-01", "hay mghib arabi");
+        // em.persist(recruiter);
 
-        em.getTransaction().commit();   
-        em.close();
-        emf.close();
+        // em.getTransaction().commit();   
+        // em.close();
+        // emf.close();
+
+
+        String toEmail = "daali.22.ssss@gmail.com"; 
+        String subject = "Test Email";
+        String body = "Hello, this is a test email sent from the Employee application.";
+
+         try {
+            // Send the email using the EmailService class
+            EmailService.sendEmail(toEmail, subject, body);
+            System.out.println("Test email sent successfully!");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            System.out.println("Error occurred while sending email.");
+        }
     }
 }
